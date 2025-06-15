3e03e8f213448cc869ae6a2080f661ee94a5c21b
juciano <jluciano85@yahoo.com.br>
2025-06-15 03:50:13 +0000

    ajustando msg para teste pipeline

--- client/src/App.js
+++ client/src/App.js
@@ -100,7 +100,7 @@ function App() {
                   onToggle={toggleReminder}
                 />
               ) : (
-                "Nenhuma tarefa até agora"
+                "Nenhuma tarefa por aqui ainda"
               )}
             </>
           )}
